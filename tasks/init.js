d7e2167d7e07aab75bfe0b60f7d3a51c13a0494f
Sergio <sergiohardov@gmail.com>
2023-03-27 21:47:01 +0300

    В функцию init добавлено создание базовых папок и файлов для SCSS

--- tasks/init.js
+++ tasks/init.js
@@ -4,17 +4,56 @@ const fs = require("fs");
 
 const messages = {
   html: "<!-- Базовый файл HTML, использовать !+tab что бы начать (пустой файл browsersync обновлять не будет) -->\r\n",
+  scss: {
+    base: "// Базовый файл стилей, все импорты подключаются тут.\r\n",
+    imports: `@import "_variables";\n@import "_mixins";\n@import "../components/**/*.scss";\n`,
+    variables: "// Базовый файл для переменных.\r\n",
+    mixins: "// Базовый файл для миксинов.\r\n",
+  },
 };
 
 module.exports = function init(done) {
   fs.mkdirSync(paths.src.html, { recursive: true });
   fs.mkdirSync(paths.src.html_components, { recursive: true });
   fs.mkdirSync(paths.src.html_pages, { recursive: true });
+  fs.mkdirSync(paths.src.scss, { recursive: true });
+  fs.mkdirSync(paths.src.scss_components, { recursive: true });
+  fs.mkdirSync(paths.src.scss_global, { recursive: true });
 
   if (!fs.existsSync(paths.src.html_pages + "/index.html")) {
     fs.writeFile(paths.src.html_pages + "/index.html", messages.html, (err) => {
       if (err) throw err;
     });
+  }
+
+  if (!fs.existsSync(paths.src.scss_global + "/style.scss")) {
+    fs.writeFile(
+      paths.src.scss_global + "/style.scss",
+      messages.scss.base + messages.scss.imports,
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
+
+  if (!fs.existsSync(paths.src.scss_global + "/_variables.scss")) {
+    fs.writeFile(
+      paths.src.scss_global + "/_variables.scss",
+      messages.scss.variables,
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
+
+  if (!fs.existsSync(paths.src.scss_global + "/_mixins.scss")) {
+    fs.writeFile(
+      paths.src.scss_global + "/_mixins.scss",
+      messages.scss.mixins,
+      (err) => {
+        if (err) throw err;
+      }
+    );
   }
 
   notify(["init"], "ok", "Базовые папки и файлы были созданы.");
