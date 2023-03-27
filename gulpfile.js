ae1593d25708086563f44b3cce100144e9cae464
Sergio <sergiohardov@gmail.com>
2023-03-27 20:01:22 +0300

    Создана функция для компиляции html (шаблонизация, форматирование). Добавлена в gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,4 @@ const tasks = requiredir("./tasks");
 exports.init = tasks.init;
 exports.cleaner = tasks.cleaner;
 exports.browsersync = tasks.browsersync;
+exports.html = tasks.html;
---
+++ tasks/html.js
@@ -0,0 +1,24 @@
+const paths = require("./_paths");
+const { notify } = require("./_helpers");
+const { src, dest } = require("gulp");
+const include = require("gulp-file-include");
+const prettify = require("gulp-prettify");
+
+module.exports = function html() {
+  let hasError = false;
+  return src(paths.src.html_pages + "/**/*.html")
+    .pipe(include())
+    .on("error", function (err) {
+      notify(["html", "compile"], "error", `Ошибка компиляции. Сообщение:`);
+      console.log(err.message);
+      hasError = true;
+      this.emit("end");
+    })
+    .pipe(prettify({ indent_size: 2 }))
+    .pipe(dest(paths.dist.folder))
+    .on("finish", function () {
+      if (!hasError) {
+        notify(["html", "compile"], "ok", "Все файлы скомпилированы.");
+      }
+    });
+};
