c442bd451402ee1afcdf0b922ace19824209b402
Sergio Hardov <sergiohardov@gmail.com>
2023-03-29 17:16:40 +0300

    Глобальный перенос на другую сборку gulp, с поддержкой ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,53 @@
-const { series, parallel } = require("gulp");
-const requiredir = require("require-dir");
-const tasks = requiredir("./tasks");
+import gulp from "gulp";
+import { path } from "./gulp/config/path.js";
+import { plugins } from "./gulp/config/plugins.js";
 
-exports.init = tasks.init;
-exports.cleaner = tasks.cleaner;
-exports.html = tasks.html.html_compile;
-exports.style = tasks.style;
-exports.script = tasks.script;
-exports.fonts = tasks.fonts.fonts_compile;
-exports.images = tasks.images;
-// exports.plugins = tasks.plugins;
-exports.watch = tasks.watch;
-exports.browsersync = tasks.browsersync;
+global.app = {
+  isBuild: process.argv.includes("--build"),
+  isDev: !process.argv.includes("--build"),
+  path: path,
+  gulp: gulp,
+  plugins: plugins,
+};
 
-exports.default = series(
-  exports.cleaner,
-  exports.html,
-  exports.fonts,
-  exports.style,
-  exports.script,
-  exports.images,
-  // exports.plugins,
-  parallel(exports.watch, exports.browsersync)
+import { init } from "./gulp/task/init.js";
+import { reset } from "./gulp/task/reset.js";
+import { html } from "./gulp/task/html.js";
+import { server } from "./gulp/task/server.js";
+import { scss } from "./gulp/task/scss.js";
+import { js } from "./gulp/task/js.js";
+import { img } from "./gulp/task/img.js";
+import { svgsprite } from "./gulp/task/svgsprite.js";
+import { otfToTtf, ttfToWoff, fontStyle } from "./gulp/task/fonts.js";
+import { zip } from "./gulp/task/zip.js";
+
+function watcher() {
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.scss, scss);
+  gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.img, img);
+}
+
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontStyle);
+
+const mainTasks = gulp.series(
+  fonts,
+  gulp.parallel(
+    html,
+    scss,
+    js,
+    img,
+    svgsprite
+  )
 );
+
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const build = gulp.series(reset, mainTasks);
+const deployZIP = gulp.series(reset, mainTasks, zip);
+
+export { init };
+export { dev };
+export { build };
+export { deployZIP };
+
+gulp.task("default", dev);
---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+// Базовый файл скриптов, входящий.
+import * as flsFunctions from "./modules/functions.js";
+flsFunctions.isWebp();
