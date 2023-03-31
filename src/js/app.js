86da554025154fd10d1bd4f07b0539b7a4ed2f57
Sergio Hardov <sergiohardov@gmail.com>
2023-03-31 18:48:20 +0300

    Добавление скрипта валидации

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,13 @@
 // Базовый файл скриптов, входящий.
 import { isWebp } from "./modules/functions.js";
 import heroSection from "./components/hero-section.js";
+import { contactForm } from "./components/contacts.js";
 
 // Проверка на WebP
 isWebp();
 
 // HeroSection
 heroSection.sectionSlider(".hero-section", ".member-card", 1200);
+
+// ContactSection
+contactForm(".contact-form");
