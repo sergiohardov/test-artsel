01321774153af3231118e343f94c8cd7b3dd49a3
Sergio Hardov <sergiohardov@gmail.com>
2023-04-01 04:57:30 +0300

    Добавление скрипта recaptcha + подключение contact секции скрипта

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,16 @@
 // Базовый файл скриптов, входящий.
-import { isWebp } from "./modules/functions.js";
+import { isWebp, reCaptchaScript } from "./modules/functions.js";
 import heroSection from "./components/hero-section.js";
-import { contactForm } from "./components/contacts.js";
+import contactSection from "./components/contact-section.js";
 
 // Проверка на WebP
 isWebp();
+
+// Подключение recaptcha script (v2)
+reCaptchaScript();
 
 // HeroSection
 heroSection.sectionSlider(".hero-section", ".member-card", 1200);
 
 // ContactSection
-contactForm(".contact-form");
+contactSection();
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,12 +1,10 @@
-
 export function isWebp() {
   function testWebp(callback) {
     const webP = new Image();
     webP.onload = webP.onerror = function () {
       callback(webP.height === 2);
     };
-    webP.src =
-      "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
   }
   // Добавление класса _webp или _no-webp для HTML
   testWebp(function (support) {
@@ -16,4 +14,12 @@ export function isWebp() {
     // FLS(support ? 'webp поддерживается' : 'webp не поддерживается')
   });
 }
-    
+
+export function reCaptchaScript() {
+  const script = document.createElement("script");
+  script.src = "https://www.google.com/recaptcha/api.js";
+  script.async = true;
+  script.defer = true;
+  document.head.appendChild(script);
+}
+
